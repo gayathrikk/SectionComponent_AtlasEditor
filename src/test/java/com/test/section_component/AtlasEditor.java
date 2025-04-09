f120735b537647bba8ccb705fead2c8bc60e8e4c
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2025-04-09 13:15:17 +0530

    Update AtlasEditor.java

--- src/test/java/com/test/section_component/AtlasEditor.java
+++ src/test/java/com/test/section_component/AtlasEditor.java
@@ -43,13 +43,13 @@ public class AtlasEditor {
 			System.out.println("Current URL: " + currentURL);
 			WebDriverWait wait = new WebDriverWait(driver, 60);
 			driver.switchTo().defaultContent(); // Switch back to default content
-			WebElement viewerElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@title='Viewer']")));
-			if (viewerElement.isEnabled() && viewerElement.isDisplayed()) {
-			viewerElement.click();
-			System.out.println("Viewer icon is clicked");
-			} else {
-			System.out.println("Viewer icon is not clickable");
-			}
+			// WebElement viewerElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@title='Viewer']")));
+			// if (viewerElement.isEnabled() && viewerElement.isDisplayed()) {
+			// viewerElement.click();
+			// System.out.println("Viewer icon is clicked");
+			// } else {
+			// System.out.println("Viewer icon is not clickable");
+			// }
 		
 		
 			String parentWindow = driver.getWindowHandle();
